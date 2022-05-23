78991ce778523467410c0ab5c91e5ed79b23d28d
Taylor <105372062+screamingandcrying@users.noreply.github.com>
2022-05-23 10:49:35 -0500

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,6 +1,8 @@
 import random
 
+word = input("Enter your word: ")
+
 s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 passlen = 8
 p =  "".join(random.sample(s,passlen ))
-print (p)
+print ("This is your new password:" + word + p)
