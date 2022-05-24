dbdb6777552b156dde657bfe02cb0fd6b6a1af2f
Taylor <105372062+screamingandcrying@users.noreply.github.com>
2022-05-23 21:28:46 -0500

    Create rockpaperscissors.py

---
+++ rockpaperscissors.py
@@ -0,0 +1,42 @@
+import random
+
+print('''Please pick one:
+            rock
+            scissors
+            paper''')
+
+while True:
+    
+    comp = ("rock", "scissors", "paper")
+    
+    options = {'rock':1, 'scissors':2, 'paper':3}
+    player_a = str(input("Player a: "))
+    comp_choice = random.choice(comp)
+    print ('Player b: ' + comp_choice)
+    if comp_choice == "rock":
+      b = 1
+    if comp_choice == "scissors":
+      b = 2
+    if comp_choice == "paper":
+      b = 3
+
+    a = options.get(player_a)
+    dif = a - b
+    
+    if dif in [-1, 2]:
+        print('player a wins.')
+        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    elif dif in [-2, 1]:
+        print('player b wins.')
+        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    else:
+        print('Draw.Please continue.')
+        print('')
